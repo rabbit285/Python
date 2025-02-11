f6d0da7ea055695494f5546a7571f85f6758ca60
rabbit285 <jacksonjarren6@gmail.com>
2025-02-11 22:03:42 +0100

    telcom

---
+++ telcom/predict.py
@@ -0,0 +1,11 @@
+import pandas as pd
+df = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+df.head()
+print(df.isnull().sum())
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.figure(figsize=(6,4))
+sns.countplot(x="Contract",hue="Churn",data=df,palette="coolwarm")
+plt.title("Churn Distribution by Contract Type")
+plt.xticks(rotation=45)
+plt.show()
