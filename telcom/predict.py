09e8d7648db646f42f296f96c00c3c6bb3b974c5
rabbit285 <jacksonjarren6@gmail.com>
2025-02-12 17:57:13 +0900

    f1

--- telcom/predict.py
+++ telcom/predict.py
@@ -4,9 +4,10 @@ df.head()
 print(df.isnull().sum())
 import matplotlib.pyplot as plt
 import seaborn as sns
-plt.figure(figsize=(6,4))
+plt.figure(figsize=(6,9))
 sns.countplot(x="Contract",hue="Churn",data=df,palette="coolwarm")
 plt.title("Churn Distribution by Contract Type")
 plt.xticks(rotation=45)
 plt.show()
-df.encode=df.copy()
+
+
